7d0060b39fe0525b46be7baa80408a3d042421e1
Sking25 <santiagorodri2509@gmail.com>
2025-03-03 20:11:53 -0500

    Creacion y correccion de los 4 formularios (falta corregir cosas)

--- src/Capa_Presentacion/Formulario_Inventario.java
+++ src/Capa_Presentacion/Formulario_Inventario.java
@@ -15,6 +15,7 @@ public class Formulario_Inventario extends javax.swing.JFrame {
         initComponents();
         ListarInventario();
         jBGrabar.setEnabled(false);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 
     public void ListarInventario(){
@@ -336,41 +337,6 @@ public class Formulario_Inventario extends javax.swing.JFrame {
         this.TFNombre.setText(jTInventario.getValueAt(rec, 1).toString());
         this.TFCantidad.setText(jTInventario.getValueAt(rec, 2).toString());
         this.CBEstado.setSelectedItem(jTInventario.getValueAt(rec, 3).toString());
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Formulario_Inventario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Formulario_Inventario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Formulario_Inventario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Formulario_Inventario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Formulario_Inventario().setVisible(true);
-            }
-        });
     }
 
     // Variables declaration - do not modify
